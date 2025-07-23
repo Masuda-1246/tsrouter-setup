2aede7e54bd31fb897dfa83ac7d6a38fe9af197e
Masuda-1246 <yaojiezengtian38@gmail.com>
2025-07-23 18:11:46 +0900

    リファクタリング

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,10 @@ import { resolve } from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
+    TanStackRouterVite({ 
+      autoCodeSplitting: true,
+      routeFileIgnorePattern: ".stories.tsx|.model.ts|.test.ts|__tests__",
+    }),
     react(),
     tailwindcss(),
   ],
